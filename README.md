9f536beadb7d4458e1eb599af20b9b32b4c3f828
ombe <h3236516@gmail.com>
2021-03-13 15:32:07 +0900

    d

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bab
-scriptable
+
+> scriptable 스크립트
+
+URLSearchParams 못써서 작동안함 바꿀예정
---
+++ index.js
@@ -0,0 +1,55 @@
+const KEY = '' // neis api key
+const ATPT_OFCDC_SC_CODE = 'J10'
+const SD_SCHUL_CODE = '7530119'
+
+const today = new Date()
+let year = today.getFullYear()
+let month = today.getMonth() + 1
+let date = today.getDate()
+const MLSV_YMD = String(year)+String(month)+String(date)
+
+const params = new URLSearchParams()
+params.append('KEY', KEY)
+params.append('Type', 'json')
+params.append('ATPT_OFCDC_SC_CODE', ATPT_OFCDC_SC_CODE)
+params.append('SD_SCHUL_CODE', SD_SCHUL_CODE)
+
+const myHeaders = new Headers()
+myHeaders.append('Content-Type', 'json')
+
+const url = `https://open.neis.go.kr/hub/mealServiceDietInfo`
+const init = {method: 'GET', headers: myHeaders, body: params}
+
+const request = new Request(url, init)
+const response = await request.loadJSON()
+
+let menu = response.mealServiceDietInfo[1].row[0].DDISH_NM
+let replacedMenu = menu.replace('<br/>', '\n')
+
+if (config.runsInWidget) {
+    try {
+        let widget = createWidget(`${date}/${month} 급식 정보`, replacedMenu)
+        Script.setWidget(widget)
+        Script.complete()
+    } catch (e) {
+        let widget = createWidget('에러', '오늘 밥 없는듯')
+        Script.setWidget(widget)
+        Script.complete()
+    }
+}
+
+
+function createWidget(title, description) {
+    let widget = new ListWidget()
+    let wTitle = widget.addText(title)
+    let wDescription = widget.addText(description)
+
+    wTitle.textColor = Color.white()
+    wTitle.font = Font.systemFont(20)
+    widget.addSpacer(5)
+    wDescription.textColor = Color.white()
+    wDescription.font = Font.systemFont(15)
+    widget.backgroundColor = new Color('#53D769')
+
+    return widget
+}
